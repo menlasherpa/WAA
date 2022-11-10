66cabf04533559e3e76f307128fb7c30b7a9883d
Lakpa Doma Sherpa <lakpadoma97@gmail.com>
2022-11-10 17:42:54 -0600

    Add files via upload

---
+++ Lab-6/src/main/java/com/example/lab1/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.lab1.service;
+
+import com.example.lab1.domain.dto.request.LoginRequest;
+import com.example.lab1.domain.dto.request.RefreshTokenRequest;
+import com.example.lab1.domain.dto.response.LoginResponse;
+
+public interface AuthService {
+    LoginResponse login(LoginRequest loginRequest);
+    LoginResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+}
